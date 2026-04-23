3b893f0053cb2c6031bd2fe5b36c003c253d7a5d
tylerd92 <twdeans@gmail.com>
2026-04-23 00:24:41 -0400

    starting to rebuild project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+      {/* <header>
+        <div className='content-container'>
+          <div className='header__content'>
+            <h1>Expensify</h1>
+            <button className='button button--link'>Logout</button>
+          </div>
+        </div>
+      </header> */}
+      <div className='box-layout'>
+        <div className='box-layout__box'>
+          <h1 className='box-layout__title'>Expensify</h1>
+          <p>It's time to get your expenses under control.</p>
+          <button className='button'>Login with Google -soon</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
