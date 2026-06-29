55cd1d488362e3ee23c0c4f96cda072be04b442e
tylerd92 <twdeans@gmail.com>
2026-06-29 15:00:01 -0400

    setup component and pages folder

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='content-container'>
+        <div className='header__content'>
+          <h1>Expensify</h1>
+          {/* <Link className="header__title" to="/dashboard">
+          <h1>Expensify</h1>
+        </Link>
+        <button className="button button--link" onClick={startLogout}>Logout</button> */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/ExpenseDashboard.tsx
@@ -0,0 +1,5 @@
+const ExpenseDashboard = () => {
+  return <div>ExpenseDashboard</div>;
+};
+
+export default ExpenseDashboard;
